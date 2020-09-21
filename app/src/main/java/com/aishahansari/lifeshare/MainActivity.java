836e73b45d7d02c550906670d07aed44393a567b
Shadab <akhtarshadab415@gmail.com>
2020-09-21 22:39:41 +0530

    Rough Layout Design

--- app/src/main/java/com/aishahansari/lifeshare/MainActivity.java
+++ app/src/main/java/com/aishahansari/lifeshare/MainActivity.java
@@ -3,6 +3,8 @@ package com.aishahansari.lifeshare;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -28,15 +30,22 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+//        To make the icon colorful as in the picture
+        navigationView.setItemIconTintList(null);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_signin)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_signin,R.id.nav_signup)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        //custom
+        Window g = getWindow();
+        g.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.TYPE_STATUS_BAR);
+//        contentView = R.layout.activity_main;
     }
 
     @Override
---
+++ app/src/main/java/com/aishahansari/lifeshare/SignUpViewModel.java
@@ -0,0 +1,7 @@
+package com.aishahansari.lifeshare;
+
+import androidx.lifecycle.ViewModel;
+
+public class SignUpViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
